610648c24e0fcd36dc73326477a58329f8438555
William Mittard <billymittard@ymail.com>
2020-04-22 22:58:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,11 +49,11 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    song_array = filename.split(" - ")
-    song_array[1] = song_array[1].chomp(".mp3")
+    song = filename.split(" - ")
+    song[1] = song_array[1].chomp(".mp3")
     song = self.new
     song.name = song_array[1]
-    song.artist_name = song_array[0]
+    song.artist_name = song[0]
     song
   end
 
