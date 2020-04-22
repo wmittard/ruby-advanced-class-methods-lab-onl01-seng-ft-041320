9ffe3976523d5e82d24ae659d2febbf2ce732314
William Mittard <billymittard@ymail.com>
2020-04-22 22:55:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,1 @@
-class Song
-  attr_accessor :name, :artist_name
-  @@all = []
-  
-  def self.create
-  song = Song.create 
-end 
-  
-  def self.all
-    @@all
-  end
 
-  def save
-    self.class.all << self
-  end
-end
